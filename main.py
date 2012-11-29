b226230566331b289739d7b7a0f442dfaa0b8fce
James Thompson <keiouu@gmail.com>
2012-11-29 13:32:30 +0000

    Check dist

--- main.py
+++ main.py
@@ -11,6 +11,29 @@
 #                                                         #
 ###########################################################
 
-print "Welcome to Arke"
+#
+# Imports
+#
+import os, platform
 
+#
+# Main Program
+#
 
+__version__ = "0.1"
+__supported_dists__ = ["Debian", "Ubuntu", "debian", "ubuntu"]
+
+print "Welcome to Arke %s!" % __version__
+print " "
+
+# Discover what system we are on, and ensure we are root
+dist, vers, name = platform.linux_distribution()
+if dist not in __supported_dists__:
+	print "Sorry, %s is not supported!" % dist
+	exit(0)
+
+print " "
+print "Running maintenance tasks..."
+
+# First we update apt
+os.system("apt-get update")
