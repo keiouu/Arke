da5e18fd15f2f323d2eac1038ffebc622fb1c378
James Thompson <keiouu@gmail.com>
2012-11-29 13:20:38 +0000

    Initial Main Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+###########################################################
+# Arke is a server management tool written in Python      #
+# It is designed to perform simple server administration  #
+# tasks and alert a sys admin to anything that requires   #
+# special attention.                                      #
+#                                                         #
+# Version: 0.1                                            #
+# Author: James Thompson                                  #
+#                                                         #
+###########################################################
+
+print "Welcome to Arke"
+
+
