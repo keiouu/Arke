4245d1663b9b161dacce3ac904275799583bd65f
James Thompson <keiouu@gmail.com>
2012-12-08 20:58:22 +0000

    Re-Try Committing 0.2

--- arke.py
+++ arke.py
@@ -6,8 +6,15 @@
 # tasks and alert a sys admin to anything that requires   #
 # special attention.                                      #
 #                                                         #
-# Version: 0.1                                            #
+# Version: 0.2                                            #
 # Author: James Thompson                                  #
+#                                                         #
+###########################################################
+#                                                         #
+# Changelist for v0.2                                     #
+#   -> Arke now runs as a Daemon                          #
+#   -> Sends notifications to central server              #
+#   -> Can accept actions from central server             #
 #                                                         #
 ###########################################################
 
@@ -15,70 +22,40 @@
 # Imports
 #
 
-import os, sys, platform, apt
-import arke_config as config
-from arke_lib import *
-from arke_lib import __hostname__
+import sys, platform
+from src import main
 
 #
 # Main Program
 #
-
-__version__ = "0.1"
 __supported_dists__ = ["Debian", "Ubuntu", "debian", "ubuntu"]
-
-print "Welcome to Arke %s!" % __version__
-print " "
 
 # Discover what system we are on
 dist, vers, name = platform.linux_distribution()
 if dist not in __supported_dists__:
-        sys.exit("\nSorry, %s is not supported!\n" % dist)
+	sys.exit("\nSorry, %s is not supported!\n" % dist)
 
 # Ensure we are root
 if not os.geteuid() == 0:
-        sys.exit("\nThis script must be run as root!\n")
+	sys.exit("\nThis script must be run as root!\n")
 
-print " "
-print "Running maintenance tasks..."
+#
+# Now the admin is out of the way.. lets see what OP wants!
+#
 
-#################
-# Update System #
-#################
-
-# First we update apt
-cache = apt.Cache()
-cache.update()
-cache.open(None)
-cache.commit()
-cache.upgrade()
-pkgs = cache.get_changes()
-
-packageVers = " -> %s" % '\n -> '.join([x.name for x in pkgs])
-packageStr = "%d packages need upgrading!" % len(pkgs)
-print "%s\n%s" % (packageStr, packageVers)
-
-if len(pkgs) > 0:
-        # What should we do with updates?
-        if config.auto_update:
-                # Print Info
-                print 'Upgrading System...'
-
-                # Lets update!
-                cache.commit()
-
-                # Send mail to notify
-                sysmail("The following packages were upgraded on %s!\n%s" % (__hostname__, packageVers))
+if __name__ == "__main__":
+        daemon = main.Arke('/tmp/arke.pid')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                else:
+                        print "Unknown command"
+                        sys.exit(2)
+                sys.exit(0)
         else:
-                # Just send a notification
-                sysmail("The following packages require upgrading on %s!\n%s" % (__hostname__, packageVers), subject="Package Upgrades")
-
-####################
-# Auto update self #
-####################
-
-if config.self_update:
-        cwd = os.getcwd()
-        os.chdir("/opt/arke/")
-        os.system("git pull")
-        os.chdir(cwd)
+                print "usage: %s start|stop|restart" % sys.argv[0]
+                sys.exit(2)
---
+++ src/main.py
@@ -0,0 +1,70 @@
+#####################
+# Main Arke Program #
+#####################
+
+
+#
+# Imports
+#
+
+import os, time, apt
+from src import *
+
+
+#
+# Main Program
+#
+
+class Arke(daemon.Daemon):
+	def run(self):
+		print " "
+		print "Welcome to Arke v0.2!"
+		print " "
+
+		while True:
+			self.perform()
+			time.sleep(config.interval)
+
+	def perform(self):
+		print "Running tasks..."
+
+		#################
+		# Update System #
+		#################
+
+		# First we update apt
+		cache = apt.Cache()
+		cache.update()
+		cache.open(None)
+		cache.commit()
+		cache.upgrade()
+		pkgs = cache.get_changes()
+
+		packageVers = " -> %s" % '\n -> '.join([x.name for x in pkgs])
+		packageStr = "%d packages need upgrading!" % len(pkgs)
+		print "%s\n%s" % (packageStr, packageVers)
+
+		if len(pkgs) > 0:
+			# What should we do with updates?
+			if config.auto_update:
+				# Print Info
+				print 'Upgrading System...'
+
+				# Lets update!
+				cache.commit()
+
+				# Send mail to notify
+				util.sysmail("The following packages were upgraded on %s!\n%s" % (util.hostname(), packageVers))
+			else:
+				# Just send a notification
+				util.sysmail("The following packages require upgrading on %s!\n%s" % (util.hostname(), packageVers), subject="Package Upgrades")
+
+		####################
+		# Auto update self #
+		####################
+
+		if config.self_update:
+			cwd = os.getcwd()
+			os.chdir("/opt/arke/")
+			os.system("git pull")
+			os.chdir(cwd)
